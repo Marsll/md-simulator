129ab232279c54b225bd2f0b52cabbaa1fd5326f
Marsll <Marsll@users.noreply.github.com>
2018-01-01 18:07:03 +0100

    Add create_nb_order API function

--- mdsimulator/neighbor_order_pbc.py
+++ mdsimulator/neighbor_order_pbc.py
@@ -8,14 +8,23 @@ of interactions."""
 import numpy as np
 import itertools
 
+def get_n_cells(box, cell_width):
+    n_cells = np.floor(box / cell_width).astype(np.int)
+    return n_cells
 
-def get_neighbors(box):
+def create_cell_array(n_cells)
+    total_n_cells = np.prod(n_cells)
+    cell_array = np.arange(total_n_cells)
+    cell_array = box.reshape(n_cells)
+    return cell_array
+
+def get_neighbors(cell_array):
     dim = len(box.shape)       # number of dimensions
     offsets = [0, -1, 1]     # offsets, 0 first so the original entry is first
     columns = []
     # equivalent to dim nested loops over offsets
     for shift in itertools.product(offsets, repeat=dim):
-        columns.append(np.roll(box, shift, np.arange(dim)[:]).ravel())
+        columns.append(np.roll(cell_array, shift, np.arange(dim)).ravel())
     neighbors = np.stack(columns, axis=-1)
     # All neighbor pairs are accounted for twice, thus, we mask every
     # redudant occurence with -1
@@ -30,4 +39,10 @@ def get_neighbors(box):
     for row in neighbors:
         row = np.unique(row)
         nbs.append(row[row != -1])
+    return nbs
+
+def create_nb_order(box, cell_width):
+    n_cells = get_n_cells(box, cell_width)
+    cell_array = create_cell_array(n_cells)
+    nbs = get_neighbors(cell_array)
     return nbs
