Fix tests due to the changed units

mdsimulator/short_ranged.py
@@ -4,7 +4,7 @@ import scipy.constants as const
 from numba import jit
 
 #Transform all units to
-eps=const.epsilon_0*1e-20*const.e**-2*1e6*const.physical_constants['Avogadro constant'][0]**-1
+eps=const.epsilon_0*1e-10*const.e**-2*1e6*const.physical_constants['Avogadro constant'][0]**-1
 
 @jit
 def pair_force(r1, r2, par1, par2, sigma_c, box, r_cut, lj=True, coulomb=True):

mdsimulator/test/test_lennard_jones_periodic.py
@@ -5,6 +5,8 @@ from ..neighbor_list import NeighborList
 from ..short_ranged import pair_potential, pair_force, potentials
 import numpy as np
 import numpy.testing as npt
+import scipy.constants as const
+
 
 def test_potential_1d_0():
     pos1 = np.array([0])
@@ -38,7 +40,7 @@ def test_force_3d():
     par1 = np.ones(3)
     par2 = np.ones(3)
     sigma_c = 1
-    force = pair_force(pos1, pos2, par1 ,par2 ,sigma_c, box, r_cut=10, coulomb=False)
+    force = pair_force(pos1, pos2, par1 ,par2 ,sigma_c, box, r_cut=10, coulomb=False) / 1e10
     force_ref = 0.00030716067 * np.array([0, 4, 3]) / 5
     npt.assert_almost_equal(force, force_ref)
 

mdsimulator/test/test_short_ranged.py
@@ -6,6 +6,9 @@ from ..neighbor_list import NeighborList
 from ..neighbor_order_pbc import create_nb_order
 import numpy.testing as npt
 from scipy.special import erfc
+import scipy.constants as const
+
+eps=const.epsilon_0*1e-10*const.e**-2*1e6*const.physical_constants['Avogadro constant'][0]**-1
 
 
 def test_potential_shape():
@@ -30,7 +33,7 @@ def test_potential_shape():
         rs[count] = np.linalg.norm(ppos[0, 0] - ppos[1, 0])
         epots[count] = potentials(ppos, params, sigma_c, nl, nbs, r_cut)
         
-        epots_ref[count] = 4 * ((1 / r)**12 - (1 / r)**6) +  1 / r * erfc(r / (np.sqrt(2)))  
+        epots_ref[count] = 4 * ((1 / r)**12 - (1 / r)**6) +  1 / (4 * np.pi * eps * r) * erfc(r / (np.sqrt(2)))  
         
         count += 1
         r += 10. / n